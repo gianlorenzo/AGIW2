e834f6f3dfcc89a0f124392ed7f31e2d8452e3c4
leonardo <leonardo.a@hotmail.it>
2018-06-13 13:57:28 +0200

    added crowdfind

---
+++ CrowdFind/CrowdFind.py
@@ -0,0 +1,16 @@
+from random import random
+
+#A seconda della selettività e dell'errore del worker, dà una risposta positiva o negativa
+def giveAnAnswer(err,item):
+    if (random()<=err):
+        return not item.assumption()
+    else:
+        return item.assumption()
+
+def crowdSourcing(err, item):
+    if (giveAnAnswer(err,item) == 1):
+        item.n1 = item.n1 + 1
+    else:
+        item.n2 = item.n2 + 1
+
+
