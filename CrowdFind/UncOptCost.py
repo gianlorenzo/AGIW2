3bad41de4eaf4051f1a93b2ae99678425b1c8fa0
leonardo <leonardo.a@hotmail.it>
2018-06-13 16:58:42 +0200

    versione 0.000001

--- CrowdFind/UncOptCost.py
+++ CrowdFind/UncOptCost.py
@@ -1,22 +1,45 @@
 from Utilities import Cost as c
+from CrowdFind import CrowdSourcing as cs
+from Items import Item
 
 
+array = []
+for i in range(0,5):
+    x = Item.Item(0.8)
+    array.append(x)
 
-def uncOptCost(items,K1,err,select,m1,m2,max,unc):
+
+def uncOptCost(items,K1,select,err,m1,m2,max,unc):
     L = []
     U = []
     y00 = c.Y00(err, select, 0, max, m1, m2, unc)
     for i in items:
+        i.setCost(y00)
+        i.setSelectivity(select)
         U.append(i)
     for x in U:
         while(len(L)<K1):
             sorted(U for x.expCost in U)
             newList = U[0:K1]
             for w in newList:
-                costoCorrente = c.Y(0.33,0.8,0,0,7,2,y00)
+                cs.crowdSourcing(err,w)
+                print("n1: " + str(w.n1)+" n2: "+str(w.n2))
+                costoCorrente = c.Y(err,select,w.n1,w.n2,m1,m2,y00)
                 if (costoCorrente==0):
-                    K1 = K1-1
                     U.remove(w)
+                    L.append(w)
+                else:
+                    w.setCost(costoCorrente)
+    return L
+
+
+
+
+
+
+list = uncOptCost(array,5,0.8,0.3,5,3,5,0.01)
+for i in list:
+    i.toString()
 
 
 
